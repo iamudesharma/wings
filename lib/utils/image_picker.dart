cfeb101b1fbd6941e0ccc53a1fb0136cef932624
iamudesharma <udesh258@gmail.com>
2022-06-21 21:37:47 +0530

    image_picker

--- lib/utils/image_picker.dart
+++ lib/utils/image_picker.dart
@@ -1,5 +1,5 @@
 import 'package:image_picker/image_picker.dart';
 
-Future<XFile?> PickerImage(ImageSource source) async {
+Future<XFile?> pickerImage(ImageSource source) async {
   return await ImagePicker().pickImage(source: source);
 }
