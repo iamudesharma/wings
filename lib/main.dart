fdddcb50323170aa45b227f98ceee5e2205d19c9
iamudesharma <udesh2568@gmail.com>
2022-07-17 21:35:06 +0530

    ios login fixed

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
+    name: "wings",
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
